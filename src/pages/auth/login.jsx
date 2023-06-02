6348a18bb4f6181cc23c2229ec547cfc14e13342
yusuf n <yusufnurm@gmail.com>
2023-06-02 18:20:10 +0700

    fix responsive

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,52 +1,60 @@
 export default function Login() {
     return (
-        <div className="h-screen font-body flex flex-col md:flex-row" >
-            <div className="flex items-center justify-center p-3 px-20 sm:p-12 md:w-1/2" >
-                <div className="w-full" >
-                    <div className="flex justify-center" >
-                        <img src="/logo-sirenta.png" className="h-20 mb-10 text-blue-600" fill="none">
-                        </img >
-                    </div >
-                    <h1 className="mb-4 text-2xl font-semibold text-center text-gray-600" >
-                        Selamat Datang
-                    </h1 >
-                    <p className="text-center font-light text-gray-600">
-                        Masuk menggunakan NIM dan Password dari akun I-LAB <br />
-                        infotech.umm.ac.id
-                    </p>
+        <div class="flex min-h-full font-body">
+          <div class="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+            <div class="mx-auto w-full max-w-sm lg:w-96">
+              <div className="flex justify-center mb-10">
+                <img 
+                  class="h-20 w-auto" 
+                  src="/logo-sirenta.png"
+                  alt="Sirenta"
+                />
+              </div>
+
+              <div className="text-center">
+                <h1 className="mb-4 text-3xl font-semibold text-center text-third" >
+                    Selamat Datang
+                </h1>
+
+                <p className="text-center text-xs font-normal text-gray-600">
+                        Masuk menggunakan NIM dan Password dari akun I-LAB    
+                </p>
+                <a className="text-center text-xs font-normal text-gray-600 underline underline-offset-1" href="https://infotech.umm.ac.id/" target="_blank" rel="noopener noreferrer">infotech.umm.ac.id</a>
+              </div>
+
+              <div class="mt-10">
+                <div>
+                  <form action="#" method="POST" class="space-y-6">
                     <div>
-                        <label className="block text-sm">
-                            Nomor Induk Mahasiswa (NIM)
-                        </label>
-                        <input type="email"
-                            className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
-                            placeholder="" />
-                    </div >
+                      <label for="nim" class="block text-sm font-semibold text-third">Nomor Induk Mahasiswa (NIM)</label>
+                      <div class="mt-1">
+                        <input id="nim" type="text" required class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" />
+                      </div>
+                    </div>
+
                     <div>
-                        <label className="block mt-4 text-sm">
-                            Password
-                        </label>
-                        <input
-                            className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"
-                            placeholder="" type="password" />
-                    </div >
+                    <label for="password" class="block text-sm font-semibold text-third">Password</label>
+                      <div class="mt-1">
+                        <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" />
+                      </div>
+                    </div>
 
+                    <div>
+                      <button type="submit" class="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Sign in</button>
+                    </div>
+                  </form>
+                </div>
 
-                    <button
-                        className="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-primary border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue"
-                        href="dashboard" >
-                        Log in
-                    </button >
-                    <p className="mt-4 text-center font-extralight" >
-                        Butuh bantuan? hubungi tim aslab
-                    </p >
-
-                </div >
-            </div >
-            <div className="h-32 md:h-auto md:w-1/2 bg-bgLight" >
-                <img src="/login.png" alt="" />
-            </div >
-
-        </div >
+              </div>
+            </div>
+          </div>
+          <div class="relative hidden w-0 flex-1 lg:block bg-bgLight">
+            <img 
+              class="absolute inset-0 h-full w-full object-cover" 
+              src="/login.png"
+              alt=""
+            />
+          </div>
+        </div>
     )
 }

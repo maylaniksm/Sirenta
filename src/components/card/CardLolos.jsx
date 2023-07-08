b7b70712aaf9bb8b59cdeebcb6303405649ff176
yusuf n <yusufnurm@gmail.com>
2023-07-08 17:07:35 +0700

    component progress

---
+++ src/components/card/CardLolos.jsx
@@ -0,0 +1,20 @@
+export default function CardLolos() {
+    return(
+        <>
+            <div>
+                <center>
+                    <img className="w-auto h-56" src="/lolos.png" alt="" srcset="" />
+                    <h1 className="font-semibold md:text-2xl text-md">Selamat, Anda dinyatakan lolos tahap ini !</h1>
+                    <div className="mt-5">
+                        <p className="md:text-lg text-sm">
+                            Jangan bersenang dulu, dan segera persiapkan diri kamu <br />
+                            untuk tahap berikutnya !<br />
+                            Silahkan cek Email, Whatsapp dan Telegram<br />
+                            secara berkala untuk a lebih lanjut
+                        </p>
+                    </div>
+                </center>
+            </div>
+        </>
+    )
+}
---
+++ src/components/card/CardWait.jsx
@@ -0,0 +1,20 @@
+export default function CardWait() {
+    return(
+        <>
+            <div>
+                <center>
+                    <img className="w-auto h-56" src="/wait.png" alt="" srcset="" />
+                    <h1 className="font-semibold md:text-2xl text-md">Pengumuman</h1>
+                    <div className="mt-5">
+                        <p className="md:text-lg text-sm">
+                            Berikut Folder Tahap 3: <br /> https://drive.google.com/drive/folders/1obIzZw-S3uBT6Wpnfkk5uMqyvwGVUyZm <br />
+                            Link Grup Tahap 3: <br /> https://chat.whatsapp.com/JnYDsXzTtcJKWgEqzNaqqB<br />
+                            TAHAP 3 AKAN BERDURASI KURANG LEBIH 3 BULAN Terdiri dari 3 kategori: <br />1. Workshop 2. Project 3. Magang<br />
+                            Teknis lebih detailnya silahkan tunggu informasi lebih lanjut dan cek email dan IG labit.umm secara berkala.
+                        </p>
+                    </div>
+                </center>
+            </div>
+        </>
+    )
+}
